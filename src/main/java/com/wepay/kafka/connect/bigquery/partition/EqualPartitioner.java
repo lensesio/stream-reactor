7688893be6b51dc01f2c6f87364057fbbba052c9
Ryan Parman <ryan@ryanparman.com>
2016-08-23 01:18:29 -0700

    Spell WePay correctly. Ensure the Bash interpreter is called uniformly.

--- src/main/java/com/wepay/kafka/connect/bigquery/partition/EqualPartitioner.java
+++ src/main/java/com/wepay/kafka/connect/bigquery/partition/EqualPartitioner.java
@@ -1,14 +1,14 @@
 package com.wepay.kafka.connect.bigquery.partition;
 
 /*
- * Copyright 2016 Wepay, Inc.
- * 
+ * Copyright 2016 WePay, Inc.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
