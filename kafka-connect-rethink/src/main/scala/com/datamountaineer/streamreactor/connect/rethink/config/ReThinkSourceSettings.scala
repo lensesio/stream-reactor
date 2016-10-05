8bf2b0fb45095d5b5d681b8927a84bddf926f7f4
Andrew Stevenson <andrew@datamountaineer.com>
2016-10-05 15:29:32 +0200

    adding rethink source

--- kafka-connect-rethink/src/main/scala/com/datamountaineer/streamreactor/connect/rethink/config/ReThinkSourceSettings.scala
+++ kafka-connect-rethink/src/main/scala/com/datamountaineer/streamreactor/connect/rethink/config/ReThinkSourceSettings.scala
@@ -27,9 +27,7 @@ import com.datamountaineer.streamreactor.connect.errors.{ErrorPolicy, ErrorPolic
   */
 case class ReThinkSourceSettings (db : String,
                                   routes: Set[Config],
-                                  tableTopicMap : Map[String, String],
-                                  errorPolicy: ErrorPolicy = new ThrowErrorPolicy,
-                                  maxRetries : Int)
+                                  tableTopicMap : Map[String, String])
 
 
 object ReThinkSourceSettings {
@@ -38,10 +36,7 @@ object ReThinkSourceSettings {
     require(raw != null && !raw.isEmpty,  s"No ${ReThinkSourceConfig.IMPORT_ROUTE_QUERY} provided!")
     val routes = raw.split(";").map(r => Config.parse(r)).toSet
     val tableTopicMap = routes.map(rm => (rm.getSource, rm.getTarget)).toMap
-    val db = config.getString(ReThinkSinkConfig.RETHINK_DB)
-    val errorPolicyE = ErrorPolicyEnum.withName(config.getString(ReThinkSinkConfig.ERROR_POLICY).toUpperCase)
-    val errorPolicy = ErrorPolicy(errorPolicyE)
-    val maxRetries = config.getInt(ReThinkSinkConfig.NBR_OF_RETRIES)
-    ReThinkSourceSettings(db, routes, tableTopicMap, errorPolicy, maxRetries)
+    val db = config.getString(ReThinkSourceConfig.RETHINK_DB)
+    ReThinkSourceSettings(db, routes, tableTopicMap)
   }
 }
