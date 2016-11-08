ae90ab8d7414ac6aef2f330a14f92d0611e5aff9
Moira Tagle <moirat@wepay.com>
2016-11-08 13:45:23 -0800

    I-62: More logging when pausing partitions (#67)
    Log how long a partition has been running when it has been paused, how long it has been paused when it is resumed, and the buffer size when it is paused.

--- kcbq-connector/src/main/java/com/wepay/kafka/connect/bigquery/BigQuerySinkTask.java
+++ kcbq-connector/src/main/java/com/wepay/kafka/connect/bigquery/BigQuerySinkTask.java
@@ -77,6 +77,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
 /**
  * A {@link SinkTask} used to translate Kafka Connect {@link SinkRecord SinkRecords} into BigQuery
@@ -97,6 +98,9 @@ public class BigQuerySinkTask extends SinkTask {
   private BatchWriterManager batchWriterManager;
   private Map<TableId, String> baseTableIdsToTopics;
   private Map<String, TableId> topicsToBaseTableIds;
+
+  private TopicPartitionManager topicPartitionManager;
+
   private Metrics metrics;
   private Sensor rowsRead;
 
@@ -198,14 +202,6 @@ public class BigQuerySinkTask extends SinkTask {
     }
   }
 
-  // Called synchronously from flush(); no synchronization required on context
-  private void resumeAllPartitions() {
-    logger.debug("Resuming all partitions");
-    for (TopicPartition topicPartition : context.assignment()) {
-      context.resume(topicPartition);
-    }
-  }
-
   @Override
   public void flush(Map<TopicPartition, OffsetAndMetadata> offsets) {
     List<TableWriter> tableWriters = new ArrayList<>();
@@ -239,7 +235,7 @@ public class BigQuerySinkTask extends SinkTask {
                                            err);
       }
     }
-    resumeAllPartitions();
+    topicPartitionManager.resumeAll();
   }
 
   private PartitionedTableId getRecordTable(SinkRecord record) {
@@ -298,16 +294,6 @@ public class BigQuerySinkTask extends SinkTask {
     return tableRecords;
   }
 
-  // Called synchronously in put(); no synchronization on context needed
-  private void pauseAllPartitions(String topic) {
-    logger.info("Pausing all partitions for topic: {}", topic);
-    for (TopicPartition topicPartition : context.assignment()) {
-      if (topicPartition.topic().equals(topic)) {
-        context.pause(topicPartition);
-      }
-    }
-  }
-
   @Override
   public void put(Collection<SinkRecord> records) {
     Map<PartitionedTableId, List<SinkRecord>> recordsMap = getRecordsByTable(records);
@@ -333,7 +319,10 @@ public class BigQuerySinkTask extends SinkTask {
 
       buffer.addAll(tableRows);
       if (bufferFull(buffer.size())) {
-        pauseAllPartitions(baseTableIdsToTopics.get(partitionedTableId.getBaseTableId()));
+        topicPartitionManager.pause(
+            baseTableIdsToTopics.get(partitionedTableId.getBaseTableId()),
+            buffer.size()
+        );
       }
     }
     rowsRead.record(recordsMap.size());
@@ -431,6 +420,7 @@ public class BigQuerySinkTask extends SinkTask {
     tableBuffers = new HashMap<>();
     tableSchemas = new HashMap<>();
     batchWriterManager = getBatchWriterManager();
+    topicPartitionManager = new TopicPartitionManager();
   }
 
   @Override
@@ -443,5 +433,82 @@ public class BigQuerySinkTask extends SinkTask {
     String version = Version.version();
     logger.trace("task.version() = {}", version);
     return version;
+  }
+
+  private enum State {
+    PAUSED,
+    RUNNING
+  }
+
+  private class TopicPartitionManager {
+
+    private Map<TopicPartition, State> topicStates;
+    private Map<TopicPartition, Long> topicChangeMs;
+
+    public TopicPartitionManager() {
+      topicStates = new HashMap<>();
+      topicChangeMs = new HashMap<>();
+    }
+
+    public void pause(String topic, int topicBufferSize) {
+      Long now = System.currentTimeMillis();
+      Collection<TopicPartition> topicPartitions = getPartitionsForTopic(topic);
+      long oldestChangeMs = now;
+      for (TopicPartition topicPartition : topicPartitions) {
+        if (topicChangeMs.containsKey(topicPartition)) {
+          oldestChangeMs = Math.min(oldestChangeMs, topicChangeMs.get(topicPartition));
+        }
+        topicStates.put(topicPartition, State.PAUSED);
+        topicChangeMs.put(topicPartition, now);
+        context.pause(topicPartition);
+      }
+
+      logger.info("Paused all partitions for topic {} with buffer size {} after {}ms: [{}]",
+                  topic,
+                  topicBufferSize,
+                  now - oldestChangeMs,
+                  topicPartitionsString(topicPartitions));
+    }
+
+    public void resume(TopicPartition topicPartition) {
+      Long now = System.currentTimeMillis();
+      if (topicStates.containsKey(topicPartition)) {
+        if (topicStates.get(topicPartition) == State.PAUSED) {
+          logger.info("Restarting topicPartition {} from pause after {}ms",
+                      topicPartition,
+                      now - topicChangeMs.get(topicPartition));
+          topicChangeMs.put(topicPartition, now);
+        } else {
+          logger.debug("'Restarting' already running partition {}",
+                       topicPartition);
+        }
+      } else {
+        logger.info("Restarting new topicPartition {}",
+                    topicPartition);
+        topicChangeMs.put(topicPartition, now);
+      }
+      topicStates.put(topicPartition, State.RUNNING);
+      context.resume(topicPartition);
+    }
+
+    public void resumeAll() {
+      for (Map.Entry<TopicPartition, State> topicState : topicStates.entrySet()) {
+        resume(topicState.getKey());
+      }
+    }
+
+    private Collection<TopicPartition> getPartitionsForTopic(String topic) {
+      return context.assignment()
+                    .stream()
+                    .filter(topicPartition -> topicPartition.topic().equals(topic))
+                    .collect(Collectors.toList());
+    }
+
+    private String topicPartitionsString(Collection<TopicPartition> topicPartitions) {
+      List<String> topicPartitionStrings = topicPartitions.stream()
+                                                          .map(TopicPartition::toString)
+                                                          .collect(Collectors.toList());
+      return String.join(", ", topicPartitionStrings);
+    }
   }
 }
