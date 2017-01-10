1d21516976c6656cea6388b37d0a84003810aa20
Andrew Stevenson <andrew@datamountaineer.com>
2017-01-10 17:15:38 +0100

    Fixes #111

--- kafka-connect-kudu/src/main/scala/com/datamountaineer/streamreactor/connect/kudu/sink/DbHandler.scala
+++ kafka-connect-kudu/src/main/scala/com/datamountaineer/streamreactor/connect/kudu/sink/DbHandler.scala
@@ -169,7 +169,7 @@ object DbHandler extends StrictLogging with KuduConverter {
       val fieldName = f.name()
       val alias = if (mappingFields.contains(fieldName)) mappingFields(fieldName) else fieldName
       val col = fromAvro(f.schema(), alias)
-      val default = if (f.defaultValue() != null) f.defaultValue() else null
+      val default = if (f.defaultVal() != null) f.defaultVal() else null
 
       if (pks.contains(alias)) {
         logger.info(s"Setting PK on ${f.name()} for ${config.getTarget}")
