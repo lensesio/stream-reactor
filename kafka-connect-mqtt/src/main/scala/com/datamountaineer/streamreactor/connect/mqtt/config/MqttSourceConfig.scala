2ab6cbed0dd6c4e1cc38e2da72509a85ef292deb
Tomas Fartaria <tomas.fartaria@marionete.co.uk>
2017-04-10 17:06:47 +0100

    Refactor constants in config to separate file and cleaned code

--- kafka-connect-mqtt/src/main/scala/com/datamountaineer/streamreactor/connect/mqtt/config/MqttSourceConfig.scala
+++ kafka-connect-mqtt/src/main/scala/com/datamountaineer/streamreactor/connect/mqtt/config/MqttSourceConfig.scala
@@ -26,93 +26,30 @@ import org.apache.kafka.common.config.{AbstractConfig, ConfigDef}
   * stream-reactor
   */
 object MqttSourceConfig {
-  val KCQL_CONFIG = "connect.mqtt.source.kcql"
-  private val KCQL_DOC = "Contains the Kafka Connect Query Language describing the sourced MQTT source and the target Kafka topics"
-  private val KCQL_DISPLAY = "KCQL commands"
-
-  val HOSTS_CONFIG = "connect.mqtt.hosts"
-  private val HOSTS_DOC = "Contains the MQTT connection end points."
-  private val HOSTS_DISPLAY = "Mqtt connection endpoints"
-
-  val QS_CONFIG = "connect.mqtt.service.quality"
-  private val QS_DOC = "Specifies the Mqtt quality of service"
-  private val QS_DISPLAY = "he Quality of Service (QoS) level is an agreement between sender and receiver of a message regarding the guarantees of delivering a message. There are 3 QoS levels in MQTT: 0 = At most once; 1 = At least once; 2 = Exactly once"
-
-  val USER_CONFIG = "connect.mqtt.user"
-  private val USER_DOC = "Contains the Mqtt connection user name"
-  private val USER_DISPLAY = "Username"
-
-  val PASSWORD_CONFIG = "connect.mqtt.password"
-  private val PASSWORD_DOC = "Contains the Mqtt connection password"
-  private val PASSWORD_DISPLAY = "Password"
-
-  val CLIENT_ID_CONFIG = "connect.mqtt.client.id"
-  private val CLIENT_ID_DOC = "Contains the Mqtt session client id"
-  private val CLIENT_ID_DISPLAY = "Client id"
-
-  val CONNECTION_TIMEOUT_CONFIG = "connect.mqtt.connection.timeout"
-  private val CONNECTION_TIMEOUT_DOC = "Provides the time interval to establish the mqtt connection"
-  private val CONNECTION_TIMEOUT_DISPLAY = "Connection timeout"
-  private val CONNECTION_TIMEOUT_DEFAULT = 3000
-
-  val CLEAN_SESSION_CONFIG = "connect.mqtt.connection.clean"
-  private val CLEAN_CONNECTION_DISPLAY = "Clean session"
-  private val CLEAN_CONNECTION_DEFAULT = true
-
-  val KEEP_ALIVE_INTERVAL_CONFIG = "connect.mqtt.connection.keep.alive"
-  private val KEEP_ALIVE_INTERVAL_DOC = "The keep alive functionality assures that the connection is still open and both broker and client are connected to one another. Therefore the client specifies a time interval in seconds and communicates it to the broker during the establishment of the connection. The interval is the longest possible period of time, which broker and client can endure without sending a message."
-  private val KEEP_ALIVE_INTERVAL_DISPLAY = "Keep alive interval"
-  private val KEEP_ALIVE_INTERVAL_DEFAULT = 5000
-
-  val SSL_CA_CERT_CONFIG = "connect.mqtt.connection.ssl.ca.cert"
-  private val SSL_CA_CERT_DOC = "Provides the path to the CA certificate file to use with the Mqtt connection"
-  private val SSL_CA_CERT_DISPLAY = "CA certificate file path"
-
-
-  val SSL_CERT_CONFIG = "connect.mqtt.connection.ssl.cert"
-  private val SSL_CERT_DOC = "Provides the path to the certificate file to use with the Mqtt connection"
-  private val SSL_CERT_DISPLAY = "Certificate key file path"
-
-  val SSL_CERT_KEY_CONFIG = "connect.mqtt.connection.ssl.key"
-  private val SSL_CERT_KEY_DOC = "Certificate private key file path."
-  private val SSL_CERT_KEY_DISPLAY = "Certificate private key file path"
-
-  val CONVERTER_CONFIG = "connect.mqtt.source.converters"
-  private val CONVERTER_DOC =
-    """
-      |Contains a tuple (Mqtt source topic and the canonical class name for the converter of a raw Mqtt message bytes to a SourceRecord).
-      |If the source topic is not matched it will default to the BytesConverter
-      |i.e. $mqtt_source1=com.datamountaineer.streamreactor.connect.source.converters.AvroConverter;$mqtt_source2=com.datamountaineer.streamreactor.connect.source.converters.JsonConverter""".stripMargin
-  private val CONVERTER_DISPLAY = "Converter class"
-
-  val THROW_ON_CONVERT_ERRORS_CONFIG = "connect.mqtt.converter.throw.on.error"
-  private val THROW_ON_CONVERT_ERRORS_DOC = "If set to false the conversion exception will be swallowed and everything carries on BUT the message is lost!!; true will throw the exception.Default is false."
-  private val THROW_ON_CONVERT_ERRORS_DISPLAY = "Throw error on conversion"
-  private val THROW_ON_CONVERT_ERRORS_DEFAULT = false
-
   val config: ConfigDef = new ConfigDef()
-    .define(HOSTS_CONFIG, Type.STRING, Importance.HIGH, HOSTS_DOC, "Connection", 1, ConfigDef.Width.MEDIUM, HOSTS_DISPLAY)
-    .define(USER_CONFIG, Type.STRING, null, Importance.HIGH, USER_DOC, "Connection", 2, ConfigDef.Width.MEDIUM, USER_DISPLAY)
-    .define(PASSWORD_CONFIG, Type.PASSWORD, null, Importance.HIGH, PASSWORD_DOC, "Connection", 3, ConfigDef.Width.MEDIUM, PASSWORD_DISPLAY)
-    .define(QS_CONFIG, Type.INT, Importance.MEDIUM, QS_DOC, "Connection", 4, ConfigDef.Width.MEDIUM, QS_DISPLAY)
-    .define(CONNECTION_TIMEOUT_CONFIG, Type.INT, CONNECTION_TIMEOUT_DEFAULT, Importance.LOW, CONNECTION_TIMEOUT_DOC, "Connection", 5, ConfigDef.Width.MEDIUM, CONNECTION_TIMEOUT_DISPLAY)
-    .define(CLEAN_SESSION_CONFIG, Type.BOOLEAN, CLEAN_CONNECTION_DEFAULT, Importance.LOW, CLEAN_SESSION_CONFIG, "Connection", 6, ConfigDef.Width.MEDIUM, CLEAN_CONNECTION_DISPLAY)
-    .define(KEEP_ALIVE_INTERVAL_CONFIG, Type.INT, KEEP_ALIVE_INTERVAL_DEFAULT, Importance.LOW, KEEP_ALIVE_INTERVAL_DOC, "Connection", 7, ConfigDef.Width.MEDIUM, KEEP_ALIVE_INTERVAL_DISPLAY)
-    .define(CLIENT_ID_CONFIG, Type.STRING, null, Importance.LOW, CLIENT_ID_DOC, "Connection", 8, ConfigDef.Width.MEDIUM, CLIENT_ID_DISPLAY)
+    .define(MqttSourceConfigConstants.HOSTS_CONFIG, Type.STRING, Importance.HIGH, MqttSourceConfigConstants.HOSTS_DOC, "Connection", 1, ConfigDef.Width.MEDIUM, MqttSourceConfigConstants.HOSTS_DISPLAY)
+    .define(MqttSourceConfigConstants.USER_CONFIG, Type.STRING, null, Importance.HIGH, MqttSourceConfigConstants.USER_DOC, "Connection", 2, ConfigDef.Width.MEDIUM, MqttSourceConfigConstants.USER_DISPLAY)
+    .define(MqttSourceConfigConstants.PASSWORD_CONFIG, Type.PASSWORD, null, Importance.HIGH, MqttSourceConfigConstants.PASSWORD_DOC, "Connection", 3, ConfigDef.Width.MEDIUM, MqttSourceConfigConstants.PASSWORD_DISPLAY)
+    .define(MqttSourceConfigConstants.QS_CONFIG, Type.INT, Importance.MEDIUM, MqttSourceConfigConstants.QS_DOC, "Connection", 4, ConfigDef.Width.MEDIUM, MqttSourceConfigConstants.QS_DISPLAY)
+    .define(MqttSourceConfigConstants.CONNECTION_TIMEOUT_CONFIG, Type.INT, MqttSourceConfigConstants.CONNECTION_TIMEOUT_DEFAULT, Importance.LOW, MqttSourceConfigConstants.CONNECTION_TIMEOUT_DOC, "Connection", 5, ConfigDef.Width.MEDIUM, MqttSourceConfigConstants.CONNECTION_TIMEOUT_DISPLAY)
+    .define(MqttSourceConfigConstants.CLEAN_SESSION_CONFIG, Type.BOOLEAN, MqttSourceConfigConstants.CLEAN_CONNECTION_DEFAULT, Importance.LOW, MqttSourceConfigConstants.CLEAN_SESSION_CONFIG, "Connection", 6, ConfigDef.Width.MEDIUM, MqttSourceConfigConstants.CLEAN_CONNECTION_DISPLAY)
+    .define(MqttSourceConfigConstants.KEEP_ALIVE_INTERVAL_CONFIG, Type.INT, MqttSourceConfigConstants.KEEP_ALIVE_INTERVAL_DEFAULT, Importance.LOW, MqttSourceConfigConstants.KEEP_ALIVE_INTERVAL_DOC, "Connection", 7, ConfigDef.Width.MEDIUM, MqttSourceConfigConstants.KEEP_ALIVE_INTERVAL_DISPLAY)
+    .define(MqttSourceConfigConstants.CLIENT_ID_CONFIG, Type.STRING, null, Importance.LOW, MqttSourceConfigConstants.CLIENT_ID_DOC, "Connection", 8, ConfigDef.Width.MEDIUM, MqttSourceConfigConstants.CLIENT_ID_DISPLAY)
 
 
     //ssl
-    .define(SSL_CA_CERT_CONFIG, Type.STRING, null, Importance.MEDIUM, SSL_CA_CERT_DOC, "SSL", 1, ConfigDef.Width.MEDIUM, SSL_CA_CERT_DISPLAY)
-    .define(SSL_CERT_CONFIG, Type.STRING, null, Importance.MEDIUM, SSL_CERT_DOC, "SSL", 2, ConfigDef.Width.MEDIUM, SSL_CERT_DISPLAY)
-    .define(SSL_CERT_KEY_CONFIG, Type.STRING, null, Importance.MEDIUM, SSL_CERT_KEY_DOC, "SSL", 3, ConfigDef.Width.MEDIUM, SSL_CERT_KEY_DISPLAY)
+    .define(MqttSourceConfigConstants.SSL_CA_CERT_CONFIG, Type.STRING, null, Importance.MEDIUM, MqttSourceConfigConstants.SSL_CA_CERT_DOC, "SSL", 1, ConfigDef.Width.MEDIUM, MqttSourceConfigConstants.SSL_CA_CERT_DISPLAY)
+    .define(MqttSourceConfigConstants.SSL_CERT_CONFIG, Type.STRING, null, Importance.MEDIUM, MqttSourceConfigConstants.SSL_CERT_DOC, "SSL", 2, ConfigDef.Width.MEDIUM, MqttSourceConfigConstants.SSL_CERT_DISPLAY)
+    .define(MqttSourceConfigConstants.SSL_CERT_KEY_CONFIG, Type.STRING, null, Importance.MEDIUM, MqttSourceConfigConstants.SSL_CERT_KEY_DOC, "SSL", 3, ConfigDef.Width.MEDIUM, MqttSourceConfigConstants.SSL_CERT_KEY_DISPLAY)
 
 
     //kcql
-    .define(KCQL_CONFIG, Type.STRING, Importance.HIGH, KCQL_DOC, "KCQL", 1, ConfigDef.Width.MEDIUM, KCQL_DISPLAY)
+    .define(MqttSourceConfigConstants.KCQL_CONFIG, Type.STRING, Importance.HIGH, MqttSourceConfigConstants.KCQL_DOC, "KCQL", 1, ConfigDef.Width.MEDIUM, MqttSourceConfigConstants.KCQL_DISPLAY)
 
     //converter
-    .define(CONVERTER_CONFIG, Type.STRING, null, Importance.HIGH, CONVERTER_DOC, "Converter", 1, ConfigDef.Width.MEDIUM, CONVERTER_DISPLAY)
-    .define(THROW_ON_CONVERT_ERRORS_CONFIG, Type.BOOLEAN, THROW_ON_CONVERT_ERRORS_DEFAULT, Importance.HIGH, THROW_ON_CONVERT_ERRORS_DOC, "Converter", 2, ConfigDef.Width.MEDIUM, THROW_ON_CONVERT_ERRORS_DISPLAY)
+    .define(MqttSourceConfigConstants.CONVERTER_CONFIG, Type.STRING, null, Importance.HIGH, MqttSourceConfigConstants.CONVERTER_DOC, "Converter", 1, ConfigDef.Width.MEDIUM, MqttSourceConfigConstants.CONVERTER_DISPLAY)
+    .define(MqttSourceConfigConstants.THROW_ON_CONVERT_ERRORS_CONFIG, Type.BOOLEAN, MqttSourceConfigConstants.THROW_ON_CONVERT_ERRORS_DEFAULT, Importance.HIGH, MqttSourceConfigConstants.THROW_ON_CONVERT_ERRORS_DOC, "Converter", 2, ConfigDef.Width.MEDIUM, MqttSourceConfigConstants.THROW_ON_CONVERT_ERRORS_DISPLAY)
+
 
 }
 
---
+++ kafka-connect-mqtt/src/main/scala/com/datamountaineer/streamreactor/connect/mqtt/config/MqttSourceConfigConstants.scala
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2017 Datamountaineer.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.datamountaineer.streamreactor.connect.mqtt.config
+
+/**
+  * Created by tomasfartaria on 10/04/2017.
+  */
+object MqttSourceConfigConstants {
+  val KCQL_CONFIG = "connect.mqtt.source.kcql"
+  val HOSTS_CONFIG = "connect.mqtt.hosts"
+  val QS_CONFIG = "connect.mqtt.service.quality"
+  val USER_CONFIG = "connect.mqtt.user"
+  val PASSWORD_CONFIG = "connect.mqtt.password"
+  val CLIENT_ID_CONFIG = "connect.mqtt.client.id"
+  val CONNECTION_TIMEOUT_CONFIG = "connect.mqtt.connection.timeout"
+  val CLEAN_SESSION_CONFIG = "connect.mqtt.connection.clean"
+  val KEEP_ALIVE_INTERVAL_CONFIG = "connect.mqtt.connection.keep.alive"
+  val SSL_CA_CERT_CONFIG = "connect.mqtt.connection.ssl.ca.cert"
+  val SSL_CERT_CONFIG = "connect.mqtt.connection.ssl.cert"
+  val SSL_CERT_KEY_CONFIG = "connect.mqtt.connection.ssl.key"
+  val CONVERTER_CONFIG = "connect.mqtt.source.converters"
+  val THROW_ON_CONVERT_ERRORS_CONFIG = "connect.mqtt.converter.throw.on.error"
+
+  private[config] val KCQL_DOC = "Contains the Kafka Connect Query Language describing the sourced MQTT source and the target Kafka topics"
+  private[config] val KCQL_DISPLAY = "KCQL commands"
+  private[config] val HOSTS_DOC = "Contains the MQTT connection end points."
+  private[config] val HOSTS_DISPLAY = "Mqtt connection endpoints"
+  private[config] val QS_DOC = "Specifies the Mqtt quality of service"
+  private[config] val QS_DISPLAY = "he Quality of Service (QoS) level is an agreement between sender and receiver of a message regarding the guarantees of delivering a message. There are 3 QoS levels in MQTT: 0 = At most once; 1 = At least once; 2 = Exactly once"
+  private[config] val USER_DOC = "Contains the Mqtt connection user name"
+  private[config] val USER_DISPLAY = "Username"
+  private[config] val PASSWORD_DOC = "Contains the Mqtt connection password"
+  private[config] val PASSWORD_DISPLAY = "Password"
+  private[config] val CLIENT_ID_DOC = "Contains the Mqtt session client id"
+  private[config] val CLIENT_ID_DISPLAY = "Client id"
+  private[config] val CONNECTION_TIMEOUT_DOC = "Provides the time interval to establish the mqtt connection"
+  private[config] val CONNECTION_TIMEOUT_DISPLAY = "Connection timeout"
+  private[config] val CONNECTION_TIMEOUT_DEFAULT = 3000
+  private[config] val CLEAN_CONNECTION_DISPLAY = "Clean session"
+  private[config] val CLEAN_CONNECTION_DEFAULT = true
+  private[config] val KEEP_ALIVE_INTERVAL_DOC = "The keep alive functionality assures that the connection is still open and both broker and client are connected to one another. Therefore the client specifies a time interval in seconds and communicates it to the broker during the establishment of the connection. The interval is the longest possible period of time, which broker and client can endure without sending a message."
+  private[config] val KEEP_ALIVE_INTERVAL_DISPLAY = "Keep alive interval"
+  private[config] val KEEP_ALIVE_INTERVAL_DEFAULT = 5000
+  private[config] val SSL_CA_CERT_DOC = "Provides the path to the CA certificate file to use with the Mqtt connection"
+  private[config] val SSL_CA_CERT_DISPLAY = "CA certificate file path"
+  private[config] val SSL_CERT_DOC = "Provides the path to the certificate file to use with the Mqtt connection"
+  private[config] val SSL_CERT_DISPLAY = "Certificate key file path"
+  private[config] val SSL_CERT_KEY_DOC = "Certificate private [config] key file path."
+  private[config] val SSL_CERT_KEY_DISPLAY = "Certificate private [config] key file path"
+  private[config] val CONVERTER_DOC =
+    """
+      |Contains a tuple (Mqtt source topic and the canonical class name for the converter of a raw Mqtt message bytes to a SourceRecord).
+      |If the source topic is not matched it will default to the BytesConverter
+      |i.e. $mqtt_source1=com.datamountaineer.streamreactor.connect.source.converters.AvroConverter;$mqtt_source2=com.datamountaineer.streamreactor.connect.source.converters.JsonConverter""".stripMargin
+  private[config] val CONVERTER_DISPLAY = "Converter class"
+  private[config] val THROW_ON_CONVERT_ERRORS_DOC = "If set to false the conversion exception will be swallowed and everything carries on BUT the message is lost!!; true will throw the exception.Default is false."
+  private[config] val THROW_ON_CONVERT_ERRORS_DISPLAY = "Throw error on conversion"
+  private[config] val THROW_ON_CONVERT_ERRORS_DEFAULT = false
+}
