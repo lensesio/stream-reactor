e63b7459053b05615ba00700d1d78a3874dda976
goncalocastro <goncaloccastro@gmail.com>
2017-04-10 17:52:24 +0100

    Fixes to the multiline string

--- kafka-connect-influxdb/src/main/scala/com/datamountaineer/streamreactor/connect/influx/config/InfluxSinkConfigConstants.scala
+++ kafka-connect-influxdb/src/main/scala/com/datamountaineer/streamreactor/connect/influx/config/InfluxSinkConfigConstants.scala
@@ -42,7 +42,8 @@ object InfluxSinkConfigConstants {
       |w weeks
       |INF infinite
       |
-      |Default retention is `autogen` from 1.0 onwards or `default` for any previous version""".stripMargin
+      |Default retention is `autogen` from 1.0 onwards or `default` for any previous version
+    """.stripMargin
   val RETENTION_POLICY_DEFAULT = "autogen"
 
   val KCQL_CONFIG = "connect.influx.sink.kcql"
@@ -50,11 +51,15 @@ object InfluxSinkConfigConstants {
   val KCQL_DISPLAY = "Kafka Connect Query Language"
 
   val ERROR_POLICY_CONFIG = "connect.influx.error.policy"
-  val ERROR_POLICY_DOC: String = "Specifies the action to be taken if an error occurs while inserting the data.\n" +
-    "There are two available options: \n" + "NOOP - the error is swallowed \n" +
-    "THROW - the error is allowed to propagate. \n" +
-    "RETRY - The exception causes the Connect framework to retry the message. The number of retries is based on \n" +
-    "The error will be logged automatically"
+  val ERROR_POLICY_DOC: String =
+    """
+      |Specifies the action to be taken if an error occurs while inserting the data.
+      |There are two available options:
+      |NOOP - the error is swallowed
+      |THROW - the error is allowed to propagate.
+      |RETRY - The exception causes the Connect framework to retry the message. The number of retries is based on
+      |The error will be logged automatically
+    """.stripMargin
   val ERROR_POLICY_DEFAULT = "THROW"
 
   val ERROR_RETRY_INTERVAL_CONFIG = "connect.influx.retry.interval"
