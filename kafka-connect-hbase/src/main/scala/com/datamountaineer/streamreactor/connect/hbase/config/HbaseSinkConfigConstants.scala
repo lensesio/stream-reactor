ce5ae0d579b7ba25d8687e2559466090b3a3c390
Tomas Fartaria <tomas.fartaria@marionete.co.uk>
2017-04-10 16:39:10 +0100

    Refactor constants to separate file

---
+++ kafka-connect-hbase/src/main/scala/com/datamountaineer/streamreactor/connect/hbase/config/HbaseSinkConfigConstants.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2017 Datamountaineer.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.datamountaineer.streamreactor.connect.hbase.config
+
+/**
+  * Created by tomasfartaria on 10/04/2017.
+  */
+object HbaseSinkConfigConstants {
+  val COLUMN_FAMILY = "connect.hbase.sink.column.family"
+  val COLUMN_FAMILY_DOC = "The hbase column family."
+
+  val EXPORT_ROUTE_QUERY = "connect.hbase.sink.kcql"
+  val EXPORT_ROUTE_QUERY_DOC = "KCQL expression describing field selection and routes."
+
+  val ERROR_POLICY = "connect.hbase.sink.error.policy"
+  val ERROR_POLICY_DOC: String =
+    """
+      |Specifies the action to be taken if an error occurs while inserting the data.
+      |There are two available options:
+      |NOOP - the error is swallowed
+      |THROW - the error is allowed to propagate. \n
+      |RETRY - The exception causes the Connect framework to retry the message. The number of retries is based on
+      |The error will be logged automatically"
+    """.stripMargin
+  val ERROR_POLICY_DEFAULT = "THROW"
+
+  val ERROR_RETRY_INTERVAL = "connect.hbase.sink.retry.interval"
+  val ERROR_RETRY_INTERVAL_DOC = "The time in milliseconds between retries."
+  val ERROR_RETRY_INTERVAL_DEFAULT = "60000"
+
+  val NBR_OF_RETRIES = "connect.hbase.sink.max.retries"
+  val NBR_OF_RETRIES_DOC = "The maximum number of times to try the write again."
+  val NBR_OF_RETIRES_DEFAULT = 20
+}
