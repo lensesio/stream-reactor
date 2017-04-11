d02d58ed3cd982b80b169e8b1ba457fe858812f8
Tomas Fartaria <tomas.fartaria@marionete.co.uk>
2017-04-11 09:38:01 +0100

    Add stripMargin to a multi-line string

--- kafka-connect-azure-documentdb/src/main/scala/com/datamountaineer/streamreactor/connect/azure/documentdb/config/DocumentDbConfigConstants.scala
+++ kafka-connect-azure-documentdb/src/main/scala/com/datamountaineer/streamreactor/connect/azure/documentdb/config/DocumentDbConfigConstants.scala
@@ -48,7 +48,7 @@ object DocumentDbConfigConstants {
       |NOOP - the error is swallowed
       |THROW - the error is allowed to propagate.
       |RETRY - The exception causes the Connect framework to retry the message. The number of retries is based on
-      |The error will be logged automatically"""
+      |The error will be logged automatically""".stripMargin
   val ERROR_POLICY_DEFAULT = "THROW"
 
   val ERROR_RETRY_INTERVAL_CONFIG = "connect.documentdb.retry.interval"
