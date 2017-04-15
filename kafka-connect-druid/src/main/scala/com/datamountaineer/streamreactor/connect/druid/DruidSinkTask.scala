387d146be72274dfa9e2b51e60c83906f833482c
shimul <shimul@tigeritbd.com>
2017-04-15 08:57:28 +0600

    fix druid dink stop logger message

--- kafka-connect-druid/src/main/scala/com/datamountaineer/streamreactor/connect/druid/DruidSinkTask.scala
+++ kafka-connect-druid/src/main/scala/com/datamountaineer/streamreactor/connect/druid/DruidSinkTask.scala
@@ -82,7 +82,7 @@ class DruidSinkTask extends SinkTask with StrictLogging {
     * Clean up Cassandra connections
     **/
   override def stop(): Unit = {
-    logger.info("Stopping Hbase sink.")
+    logger.info("Stopping Druid sink.")
     //writer.foreach(w => w.close())
   }
 
