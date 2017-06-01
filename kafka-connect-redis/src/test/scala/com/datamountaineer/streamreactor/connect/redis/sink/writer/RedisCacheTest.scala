5dd772726a0e5d1898445c4046a3d997c58322cc
stheppi <stefan.bocutiu@gmail.com>
2017-06-01 22:24:03 +0100

    adding unit test for handling key building using the incoming sinkrecord

--- kafka-connect-redis/src/test/scala/com/datamountaineer/streamreactor/connect/redis/sink/writer/RedisCacheTest.scala
+++ kafka-connect-redis/src/test/scala/com/datamountaineer/streamreactor/connect/redis/sink/writer/RedisCacheTest.scala
@@ -87,6 +87,52 @@ class RedisCacheTest extends WordSpec with Matchers with BeforeAndAfterAll with 
       map2("threshold").toString shouldBe "12.4"
     }
 
+    "write Kafka records to Redis using CACHE mode and PK field is not in the selected fields" in {
+
+      val TOPIC = "topic"
+      val TABLE_NAME_RAW = "someTable"
+      val QUERY_ALL = s"SELECT age FROM $TOPIC PK firstName"
+
+      val config = mock[RedisSinkConfig]
+      when(config.getString(RedisSinkConfigConstants.REDIS_HOST)).thenReturn("localhost")
+      when(config.getInt(RedisSinkConfigConstants.REDIS_PORT)).thenReturn(6379)
+      when(config.getString(RedisSinkConfigConstants.REDIS_PASSWORD)).thenReturn("")
+      when(config.getString(RedisSinkConfigConstants.KCQL_CONFIG)).thenReturn(QUERY_ALL)
+      when(config.getString(RedisSinkConfigConstants.ERROR_POLICY)).thenReturn("THROW")
+      val connectionInfo = new RedisConnectionInfo("localhost", 6379, None)
+      val settings = RedisSinkSettings(config)
+      val writer = new RedisCache(settings)
+
+      val schema = SchemaBuilder.struct().name("com.example.Person")
+        .field("firstName", Schema.STRING_SCHEMA)
+        .field("age", Schema.INT32_SCHEMA)
+        .field("threshold", Schema.OPTIONAL_FLOAT64_SCHEMA).build()
+
+      val struct1 = new Struct(schema).put("firstName", "Alex").put("age", 30)
+      val struct2 = new Struct(schema).put("firstName", "Mara").put("age", 22).put("threshold", 12.4)
+
+      val sinkRecord1 = new SinkRecord(TOPIC, 1, null, null, schema, struct1, 0)
+      val sinkRecord2 = new SinkRecord(TOPIC, 1, null, null, schema, struct2, 1)
+
+      writer.write(Seq(sinkRecord1, sinkRecord2))
+
+      val gson = new Gson()
+      val jedis = new Jedis(connectionInfo.host, connectionInfo.port)
+
+      val val1 = jedis.get("Alex")
+      val1 should not be null
+
+      val map1 = gson.fromJson(val1, classOf[java.util.Map[String, AnyRef]]).asScala
+      map1("age").toString shouldBe "30.0" //it gets back a java double!?
+
+      val val2 = jedis.get("Mara")
+      val2 should not be null
+
+      val map2 = gson.fromJson(val2, classOf[java.util.Map[String, AnyRef]]).asScala
+      map2("age").toString shouldBe "22.0"
+    }
+
+
     "write Kafka records to Redis using CACHE mode with explicit KEY (using INSERT)" in {
 
       val TOPIC = "topic2"
