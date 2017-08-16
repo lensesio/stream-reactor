874d2eb46a5d7a493840c04bc8dba4084001029a
stheppi <C0ntd3coding>
2017-08-16 11:19:41 +0100

    adding page header

--- kafka-connect-hbase/src/test/scala/com/datamountaineer/streamreactor/connect/hbase/writers/Combiner.scala
+++ kafka-connect-hbase/src/test/scala/com/datamountaineer/streamreactor/connect/hbase/writers/Combiner.scala
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 Datamountaineer.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.datamountaineer.streamreactor.connect.hbase.writers
 
 import org.kitesdk.minicluster.{HBaseService, HdfsService, MiniCluster, ZookeeperService}
@@ -7,7 +22,6 @@ import org.scalatest.{BeforeAndAfterAll, Suites}
   * Created by andrew@datamountaineer.com on 07/08/2017. 
   * stream-reactor
   */
-
 class Combiner extends Suites(new HbaseWriterTest, new HbaseWriterTestRetry) with BeforeAndAfterAll {
 
   var miniCluster: Option[MiniCluster] = None
