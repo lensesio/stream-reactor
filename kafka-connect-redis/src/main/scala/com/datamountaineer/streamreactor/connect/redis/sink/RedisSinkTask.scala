a2609e626009a9bf7c5f0d8fd4861027ed39874a
Andrew Stevenson <andrew@datamountaineer.com>
2017-09-04 22:26:48 +0200

    revert redis fix

--- kafka-connect-redis/src/main/scala/com/datamountaineer/streamreactor/connect/redis/sink/RedisSinkTask.scala
+++ kafka-connect-redis/src/main/scala/com/datamountaineer/streamreactor/connect/redis/sink/RedisSinkTask.scala
@@ -59,21 +59,11 @@ class RedisSinkTask extends SinkTask with StrictLogging {
     //-- Find out the Connector modes (cache | INSERT (SortedSet) | PK (SortedSetS)
 
     // Cache mode requires >= 1 PK and *NO* STOREAS SortedSet setting
-    val modeCache = settings.copy(kcqlSettings = settings.kcqlSettings.filter(_.kcqlConfig.getStoredAs == null))
-    //.filter(_.kcqlConfig.getPrimaryKeys.size() >= 1))
-
+    val modeCache = settings.copy(kcqlSettings = settings.kcqlSettings.filter(_.kcqlConfig.getStoredAs == null).filter(_.kcqlConfig.getPrimaryKeys.size() >= 1))
     // Insert Sorted Set mode requires: target name of SortedSet to be defined and STOREAS SortedSet syntax to be provided
-    val mode_INSERT_SS = settings
-                          .copy(kcqlSettings = settings
-                                                  .kcqlSettings
-                                                  .filter(_.kcqlConfig.getStoredAs.toUpperCase() == "SORTEDSET")
-                                                  .filter(_.kcqlConfig.getTarget.length > 0))
+    val mode_INSERT_SS = settings.copy(kcqlSettings = settings.kcqlSettings.filter(_.kcqlConfig.getStoredAs == "SortedSet").filter(_.kcqlConfig.getTarget.length > 0))
     // Multiple Sorted Sets mode requires: 1 Primary Key to be defined and STORE SortedSet syntax to be provided
-    val mode_PK_SS = settings
-                          .copy(kcqlSettings = settings
-                                                  .kcqlSettings
-                                                  .filter(_.kcqlConfig.getStoredAs.toUpperCase() == "SORTEDSET")
-                                                  .filter(_.kcqlConfig.getPrimaryKeys.length == 1))
+    val mode_PK_SS = settings.copy(kcqlSettings = settings.kcqlSettings.filter(_.kcqlConfig.getStoredAs == "SortedSet").filter(_.kcqlConfig.getPrimaryKeys.length == 1))
 
     //-- Start as many writers as required
     writer =
