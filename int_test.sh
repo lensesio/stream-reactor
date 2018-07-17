fe88433d4d56d16d127ff450bf11b78e2a2f275b
sksamuel <sam@sksamuel.com>
2018-07-17 13:03:10 +0100

    Int test scripts launches docker compose for component

--- int_test.sh
+++ int_test.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
 
 CONNECT_PORT=8083
-declare -a connectors=("hive", "redis")
+declare -a connectors=("hive" "redis")
+connectorslen=${#connectors[@]}
 
-for connector in "${connectors[@]}"
-do
-    echo "Running integration tests for $connector"
+for (( i=0; i<${connectorslen}; i++ )); do
+    CONNECTOR=${connectors[$i]}
+    echo "Running integration tests for $CONNECTOR"
 
-    CONNECTOR_PATH="kafka-connect-$connector"
+    CONNECTOR_PATH="kafka-connect-$CONNECTOR"
+    echo "Connector path = $CONNECTOR_PATH"
+
     IT_PATH="$CONNECTOR_PATH/it"
+    echo "IT test path = $IT_PATH"
+
     COMPOSE_FILE="$IT_PATH/docker-compose.yml"
+    echo "Compose file = $COMPOSE_FILE"
 
     # if docker is present for this component, start it up, and wait for kafka connect to initialize
-    if [ -e $COMPOSE_FILE ]
+    if [ -e "$COMPOSE_FILE" ]
     then
-        docker-compose -d -f $COMPOSE_FILE up
+        echo "Docker compose has been detected for $CONNECTOR; starting up as daemon"
+        docker-compose -f $COMPOSE_FILE up -d
 
-        for ((i=0;i<60;i++)); do
-         sleep 5
-         curl "http://localhost:$CONNECT_PORT/connector-plugins" | grep "FileStream" && break
+        for (( i=0 ; i<60 ; i++ )); do
+            sleep 5
+            curl "http://localhost:$CONNECT_PORT/connector-plugins" | grep "FileStream" && break
         done
-
     fi
 
     ./gradlew -Pintegration-tests $IT_PATH:check
 
     # if docker is present for this component, shut it down
-    if [ -e "$connector/docker-compose.yml" ]
+    if [ -e "$COMPOSE_FILE" ]
     then
         docker-compose -f $COMPOSE_FILE stop
     fi
 
-    echo "Completed integration tests for $connector"
+    echo "Completed integration tests for $CONNECTOR"
 done
