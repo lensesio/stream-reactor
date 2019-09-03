db44290d1bf265c6f0453708ece92003ecf8b5ff
stefan bocutiu <stefan.bocutiu@gmail.com>
2019-09-03 15:20:59 +0100

    Fix for writing nested structures to Hive.Adds unit test for MapValueConverter. Fix for Hive 2.x to avoid loading ridiculous old calcite-avatica bringing an old Jackson library in

--- kafka-connect-hive-1.1/src/main/scala/com/landoop/streamreactor/connect/hive/sink/evolution/StrictEvolutionPolicy.scala
+++ kafka-connect-hive-1.1/src/main/scala/com/landoop/streamreactor/connect/hive/sink/evolution/StrictEvolutionPolicy.scala
@@ -9,13 +9,13 @@ import scala.collection.JavaConverters._
 import scala.util.Try
 
 /**
-  * An implementation of [[EvolutionPolicy]] that requires the
-  * input schema to be equal to the metastore schema.
-  *
-  * This means that every field in the metastore schema must be
-  * present in the incoming records, and the incoming records
-  * cannot contain any extra fields.
-  */
+ * An implementation of [[EvolutionPolicy]] that requires the
+ * input schema to be equal to the metastore schema.
+ *
+ * This means that every field in the metastore schema must be
+ * present in the incoming records, and the incoming records
+ * cannot contain any extra fields.
+ */
 object StrictEvolutionPolicy extends EvolutionPolicy {
 
   override def evolve(dbName: DatabaseName,
@@ -23,13 +23,22 @@ object StrictEvolutionPolicy extends EvolutionPolicy {
                       metastoreSchema: Schema,
                       inputSchema: Schema)
                      (implicit client: IMetaStoreClient): Try[Schema] = Try {
-    HiveSchemas.toKafka(client.getTable(dbName.value, tableName.value))
+    val schema = HiveSchemas.toKafka(client.getTable(dbName.value, tableName.value))
+    schema
   }.map { schema =>
-    val compatible = schema.fields().asScala.forall { field =>
-      inputSchema.field(field.name) != null ||
-        field.schema().isOptional ||
-        field.schema().defaultValue() != null
+    //Hive keeps the fields in lowercase
+    val inputFields = inputSchema.fields().asScala.map { f =>
+      f.name().toLowerCase()
+    }.toSet
+    schema.fields().asScala.foreach { field =>
+      val exists = inputFields.contains(field.name)
+      val optional = field.schema().isOptional
+      val default = field.schema().defaultValue()
+      val compatible = exists || optional || default != null
+      if (!compatible) {
+        sys.error(s"Input Schema is not compatible with the metastore for field [${field.name()}]")
+      }
     }
-    if (compatible) schema else sys.error("Input Schema is not compatible with the metastore")
+    schema
   }
 }
