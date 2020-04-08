9b8f83684bddab3f73a53f66dda77d480dd5aa70
David Sloan <david.sloan@lenses.io>
2020-04-08 09:51:40 +0100

    Fixing JMS module

--- kafka-connect-jms/src/test/scala/com/datamountaineer/streamreactor/connect/sink/converters/ObjectMessageConverterTest.scala
+++ kafka-connect-jms/src/test/scala/com/datamountaineer/streamreactor/connect/sink/converters/ObjectMessageConverterTest.scala
@@ -19,10 +19,9 @@ package com.datamountaineer.streamreactor.connect.sink.converters
 import java.util.UUID
 
 import javax.jms.ObjectMessage
-import com.datamountaineer.streamreactor.connect.TestBase
+import com.datamountaineer.streamreactor.connect.{TestBase, Using}
 import com.datamountaineer.streamreactor.connect.jms.config.{JMSConfig, JMSSettings}
 import com.datamountaineer.streamreactor.connect.jms.sink.converters.ObjectMessageConverter
-import com.sksamuel.scalax.io.Using
 import org.apache.activemq.ActiveMQConnectionFactory
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.matchers.should.Matchers
