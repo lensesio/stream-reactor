4eee99ed48f2207cf66e88cf219a3c7d80a9bc27
Chris Egerton <chrise@confluent.io>
2020-10-02 14:44:02 -0400

    GH-39: Fix NPE when null inner values are encountered with field sanitization enabled (#40)

--- kcbq-connector/src/main/java/com/wepay/kafka/connect/bigquery/utils/FieldNameSanitizer.java
+++ kcbq-connector/src/main/java/com/wepay/kafka/connect/bigquery/utils/FieldNameSanitizer.java
@@ -1,5 +1,6 @@
 package com.wepay.kafka.connect.bigquery.utils;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -20,15 +21,17 @@ public class FieldNameSanitizer {
   // letters, numbers, and underscores.
   // Note: a.b and a/b will have the same value after sanitization which will cause Duplicate key
   // Exception.
+  @SuppressWarnings("unchecked")
   public static Map<String, Object> replaceInvalidKeys(Map<String, Object> map) {
-    return map.entrySet().stream().collect(Collectors.toMap(
-        (entry) -> sanitizeName(entry.getKey()),
-        (entry) -> {
-          if (entry.getValue() instanceof Map) {
-            return replaceInvalidKeys((Map<String, Object>) entry.getValue());
-          }
-          return entry.getValue();
-        }
-    ));
+    Map<String, Object> result = new HashMap<>();
+    map.forEach((key, value) -> {
+      String sanitizedKey = sanitizeName(key);
+      if (value instanceof Map) {
+        result.put(sanitizedKey, replaceInvalidKeys((Map<String, Object>) value));
+      } else {
+        result.put(sanitizedKey, value);
+      }
+    });
+    return result;
   }
 }
