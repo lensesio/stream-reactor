84c3a5558c083500eaae852a8a070054dfc8512f
johnathana <johnathana@gmail.com>
2021-03-04 20:38:13 +0200

    StreamReactor integration tests with testcontainers

---
+++ smart-build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -x
+
+MODIFIED_MODULES=$(git show --name-only --oneline HEAD | tail -n +2 | cut -d/ -f1 | sort | uniq )
+
+BUILD_ALL=false
+
+for module in ${MODIFIED_MODULES}; do
+  if [[ ${module} != kafka-connect-* || ${module} == kafka-connect-common ]]; then
+      BUILD_ALL=true
+      break
+  fi
+done
+
+if $BUILD_ALL; then
+    ./gradlew clean test
+else
+    GRADLE_TASKS=""
+    for module in ${MODIFIED_MODULES}; do
+        GRADLE_TASKS="${GRADLE_TASKS} :${module}:test"
+    done
+
+    ./gradlew clean ${GRADLE_TASKS}
+fi
