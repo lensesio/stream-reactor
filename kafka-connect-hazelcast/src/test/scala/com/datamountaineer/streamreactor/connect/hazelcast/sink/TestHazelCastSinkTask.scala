a1365e7e4a8eae81e178e16b5fdcebf5911e9447
David Sloan <david.sloan@lenses.io>
2021-12-13 12:11:13 +0000

    hazelcast logback conversion

--- kafka-connect-hazelcast/src/test/scala/com/datamountaineer/streamreactor/connect/hazelcast/sink/TestHazelCastSinkTask.scala
+++ kafka-connect-hazelcast/src/test/scala/com/datamountaineer/streamreactor/connect/hazelcast/sink/TestHazelCastSinkTask.scala
@@ -33,7 +33,7 @@ class TestHazelCastSinkTask extends TestBase with MockitoSugar {
 
   "should start SinkTask and write json" in {
     val configApp1 = new Config()
-    configApp1.setProperty( "hazelcast.logging.type", "log4j" )
+    configApp1.setProperty( "hazelcast.logging.type", "slf4j" )
     val gc = new  GroupConfig(TESTS_GROUP_NAME, HazelCastSinkConfigConstants.SINK_GROUP_PASSWORD_DEFAULT)
     configApp1.setGroupConfig(gc)
     val instance = Hazelcast.newHazelcastInstance(configApp1)
