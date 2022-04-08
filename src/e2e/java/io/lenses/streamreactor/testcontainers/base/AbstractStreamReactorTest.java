fbddcc53adffdb21135ce5e3bf2fd6b97ebf9437
Dave Bassan <dave.bassan@lenses.io>
2022-04-08 15:08:27 +0100

    upload and download assemblies for e2e test

--- src/e2e/java/io/lenses/streamreactor/testcontainers/base/AbstractStreamReactorTest.java
+++ src/e2e/java/io/lenses/streamreactor/testcontainers/base/AbstractStreamReactorTest.java
@@ -157,9 +157,8 @@ public abstract class AbstractStreamReactorTest {
 
         try {
             final String regex = String.format(".*%s%s.*.jar", connector, directorySuffix);
-            final List<Path> files = Files.find(Paths.get(String.join(File.separator,
-                            System.getProperty("user.dir"), "kafka-connect-" + connector, "target")),
-                    3,
+            final List<Path> files = Files.find(Paths.get(System.getProperty("user.dir")),
+                    5,
                     (path, basicFileAttributes) -> path.toFile().getName().matches(regex)
             ).collect(Collectors.toList());
             LOGGER.info("Files: " + files.stream().map(file -> String.join("::", file.toString())).collect(Collectors.toList()));
