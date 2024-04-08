70714e0b6f4451a3db1bea4026322d0f77b216e4
Scala Steward <scala_steward@virtuslab.com>
2024-04-08 15:46:15 +0000

    Update azure-core to 1.48.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -74,7 +74,7 @@ object Dependencies {
 
     val azureDataLakeVersion = "12.18.4"
     val azureIdentityVersion = "1.12.0"
-    val azureCoreVersion     = "1.47.0"
+    val azureCoreVersion     = "1.48.0"
     val gcpStorageVersion    = "2.37.0"
 
     val jacksonVersion      = "2.17.0"
