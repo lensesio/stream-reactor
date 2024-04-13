d55218c2b5fa978fa515046681059e06f8178667
Scala Steward <scala_steward@virtuslab.com>
2024-04-13 14:25:25 +0000

    Update elasticsearch to 8.13.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -140,7 +140,7 @@ object Dependencies {
 
     object Elastic7Versions extends ElasticVersions {
       override val elastic4sVersion:     String = "7.17.4"
-      override val elasticSearchVersion: String = "7.17.20"
+      override val elasticSearchVersion: String = "8.13.2"
       override val jnaVersion:           String = "5.14.0"
     }
 
