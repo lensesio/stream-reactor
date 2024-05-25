316c0cc882bb948449c5f2e8c09e2895e075f5c4
Scala Steward <scala_steward@virtuslab.com>
2024-05-25 15:55:20 +0000

    Update commons-compress to 1.26.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -113,7 +113,7 @@ object Dependencies {
 
     val commonsNetVersion      = "3.10.0"
     val commonsCodecVersion    = "1.17.0"
-    val commonsCompressVersion = "1.26.1"
+    val commonsCompressVersion = "1.26.2"
     val commonsConfigVersion   = "2.10.1"
     val commonsIOVersion       = "2.16.1"
     val commonsHttpVersion     = "4.5.14"
