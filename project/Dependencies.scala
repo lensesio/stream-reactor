f4a350fcf5473e9cb138645351c6eee378b1ba46
Scala Steward <scala_steward@virtuslab.com>
2024-06-30 14:36:26 +0000

    Update wiremock to 3.8.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -71,7 +71,7 @@ object Dependencies {
     val logbackVersion      = "1.5.6"
     val scalaLoggingVersion = "3.9.5"
 
-    val wiremockVersion = "3.6.0"
+    val wiremockVersion = "3.8.0"
     val parquetVersion  = "1.14.0"
 
     val jerseyCommonVersion = "3.1.7"
