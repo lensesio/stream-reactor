56653a9640b2de87238a2b1e39289626b64b3f3b
Scala Steward <scala_steward@virtuslab.com>
2024-07-22 15:32:29 +0000

    Update azure-identity to 1.13.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -80,7 +80,7 @@ object Dependencies {
     val awsSdkVersion  = "2.25.70"
 
     val azureDataLakeVersion              = "12.19.1"
-    val azureIdentityVersion              = "1.13.0"
+    val azureIdentityVersion              = "1.13.1"
     val azureCoreVersion                  = "1.49.1"
     val msal4jVersion                     = "1.16.0"
     val msal4jPersistenceExtensionVersion = "1.3.0"
