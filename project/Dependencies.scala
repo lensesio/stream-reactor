55b82ab33d049083f6819f9618c40e2505f7583a
Scala Steward <scala_steward@virtuslab.com>
2024-07-29 18:16:07 +0000

    Update dnsjava to 3.6.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -71,7 +71,7 @@ object Dependencies {
     val logbackVersion      = "1.5.6"
     val scalaLoggingVersion = "3.9.5"
 
-    val dnsJavaVersion  = "3.6.0"
+    val dnsJavaVersion  = "3.6.1"
     val wiremockVersion = "3.8.0"
     val parquetVersion  = "1.14.1"
 
