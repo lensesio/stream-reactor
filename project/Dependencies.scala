9ac528fc995291b5897fde8d5a75269c0bf64588
Scala Steward <scala_steward@virtuslab.com>
2024-08-12 22:58:59 +0000

    Update activemq-broker, activemq-client to 6.1.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -111,7 +111,7 @@ object Dependencies {
     val influxVersion = "7.2.0"
 
     val jmsApiVersion         = "3.1.0"
-    val activeMqVersion       = "6.1.2"
+    val activeMqVersion       = "6.1.3"
     val protocVersion         = "3.11.4"
     val googleProtobufVersion = "3.25.4"
 
