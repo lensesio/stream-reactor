c7f05b1e603b1649ba526dad70d4403ca61ab4c7
Scala Steward <scala_steward@virtuslab.com>
2024-08-12 22:59:43 +0000

    Update connect-json, kafka-clients to 7.7.0-ce

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -45,7 +45,7 @@ object Dependencies {
 
     val cyclopsVersion = "10.4.1"
 
-    val kafkaVersion:     String = "3.8.0"
+    val kafkaVersion:     String = "7.7.0-ce"
     val confluentVersion: String = "7.6.2"
 
     val enumeratumVersion = "1.7.4"
