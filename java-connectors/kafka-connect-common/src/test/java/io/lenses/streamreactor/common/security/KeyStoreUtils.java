26cbcc00089d894ba009ef3bc48d7dc805e56d09
David Sloan <david.sloan@lenses.io>
2024-09-11 09:28:40 +0100

    Constants

--- java-connectors/kafka-connect-common/src/test/java/io/lenses/streamreactor/common/security/KeyStoreUtils.java
+++ java-connectors/kafka-connect-common/src/test/java/io/lenses/streamreactor/common/security/KeyStoreUtils.java
@@ -48,12 +48,24 @@ import java.security.KeyStore;
 import java.security.Security;
 import java.security.cert.X509Certificate;
 import java.security.interfaces.RSAPrivateKey;
+import java.time.Duration;
 import java.time.Instant;
-import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 @Slf4j
 public class KeyStoreUtils {
+
+  private static final String COMMON_NAME = "CN";
+  private static final String DIRECTORY_SECURITY = "security";
+  private static final String ALGORITHM_RSA = "RSA";
+  private static final String PROVIDER_BC = "BC";
+  private static final int KEY_SIZE = 2048;
+  private static final int DAYS_IN_YEAR = 365;
+  private static final String SIGNER_SIGNATURE_ALGORITHM = "SHA256WithRSAEncryption";
+  private static final String KEYSTORE_TYPE = "JKS";
+  private static final String KEYSTORE_FILE = "keystore.jks";
+  private static final String TRUSTSTORE_FILE = "truststore.jks";
+  private static final String TEST_ALIAS = "alias";
 
   static {
     Security.addProvider(new BouncyCastleProvider());
@@ -61,31 +73,31 @@ public class KeyStoreUtils {
 
   public static Path createKeystore(String commonName, String keyStorePassword, String trustStorePassword)
       throws Exception {
-    Path tmpDir = Files.createTempDirectory("security");
+    Path tmpDir = Files.createTempDirectory(DIRECTORY_SECURITY);
 
-    KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA", "BC");
-    keyPairGen.initialize(2048);
+    KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance(ALGORITHM_RSA, PROVIDER_BC);
+    keyPairGen.initialize(KEY_SIZE);
     KeyPair keyPair = keyPairGen.generateKeyPair();
 
     Date notBefore = new Date(Instant.now().toEpochMilli());
-    Date notAfter = new Date(Instant.now().plus(365, ChronoUnit.DAYS).toEpochMilli());
+    Date notAfter = new Date(Instant.now().plus(Duration.ofDays(DAYS_IN_YEAR)).toEpochMilli());
 
     SubjectPublicKeyInfo publicKeyInfo = SubjectPublicKeyInfo.getInstance(keyPair.getPublic().getEncoded());
 
     X509v3CertificateBuilder certBuilder =
         new X509v3CertificateBuilder(
-            new X500Name("CN=" + commonName),
+            new X500Name(COMMON_NAME + "=" + commonName),
             BigInteger.valueOf(System.currentTimeMillis()),
             notBefore,
             notAfter,
-            new X500Name("CN=" + commonName),
+            new X500Name(COMMON_NAME + "=" + commonName),
             publicKeyInfo
         );
 
     JcaContentSignerBuilder contentSignerBuilder =
-        new JcaContentSignerBuilder("SHA256WithRSAEncryption").setProvider("BC");
+        new JcaContentSignerBuilder(SIGNER_SIGNATURE_ALGORITHM).setProvider(PROVIDER_BC);
     X509Certificate certificate =
-        new JcaX509CertificateConverter().setProvider("BC")
+        new JcaX509CertificateConverter().setProvider(PROVIDER_BC)
             .getCertificate(certBuilder.build(contentSignerBuilder.build(keyPair.getPrivate())));
 
     createAndSaveKeystore(tmpDir, keyStorePassword, certificate, (RSAPrivateKey) keyPair.getPrivate());
@@ -97,13 +109,13 @@ public class KeyStoreUtils {
 
   private static String createAndSaveKeystore(Path tmpDir, String password, X509Certificate certificate,
       RSAPrivateKey privateKey) throws Exception {
-    KeyStore keyStore = KeyStore.getInstance("JKS");
+    KeyStore keyStore = KeyStore.getInstance(KEYSTORE_TYPE);
     keyStore.load(null, password.toCharArray());
 
-    keyStore.setKeyEntry("alias", privateKey, password.toCharArray(), new java.security.cert.Certificate[]{
+    keyStore.setKeyEntry(TEST_ALIAS, privateKey, password.toCharArray(), new java.security.cert.Certificate[]{
         certificate});
 
-    String keyStorePath = tmpDir.resolve("keystore.jks").toString();
+    String keyStorePath = tmpDir.resolve(KEYSTORE_FILE).toString();
     try (FileOutputStream keyStoreOutputStream = new FileOutputStream(keyStorePath)) {
       keyStore.store(keyStoreOutputStream, password.toCharArray());
     }
@@ -113,11 +125,11 @@ public class KeyStoreUtils {
 
   private static String createAndSaveTruststore(Path tmpDir, String password, X509Certificate certificate)
       throws Exception {
-    KeyStore trustStore = KeyStore.getInstance("JKS");
+    KeyStore trustStore = KeyStore.getInstance(KEYSTORE_TYPE);
     trustStore.load(null, password.toCharArray());
 
-    trustStore.setCertificateEntry("alias", certificate);
-    String trustStorePath = tmpDir.resolve("truststore.jks").toString();
+    trustStore.setCertificateEntry(TEST_ALIAS, certificate);
+    String trustStorePath = tmpDir.resolve(TRUSTSTORE_FILE).toString();
 
     try (FileOutputStream trustStoreOutputStream = new FileOutputStream(trustStorePath)) {
       trustStore.store(trustStoreOutputStream, password.toCharArray());
