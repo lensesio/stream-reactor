a1c9348671e731877811cb64ef4a326277238da6
Scala Steward <scala_steward@virtuslab.com>
2024-09-18 17:47:49 +0000

    Update azure-storage-file-datalake to 12.21.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -80,7 +80,7 @@ object Dependencies {
     val calciteVersion = "1.34.0"
     val awsSdkVersion  = "2.27.5"
 
-    val azureDataLakeVersion              = "12.20.1"
+    val azureDataLakeVersion              = "12.21.0"
     val azureIdentityVersion              = "1.13.2"
     val azureCoreVersion                  = "1.51.0"
     val msal4jVersion                     = "1.16.0"
