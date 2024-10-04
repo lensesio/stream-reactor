978c3be2a0becf5d72ea922f710d9370f8e8f5ff
Scala Steward <scala_steward@virtuslab.com>
2024-10-04 16:13:44 +0000

    Update http4s-circe, http4s-dsl to 1.0.0-M42

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -50,7 +50,7 @@ object Dependencies {
 
     val enumeratumVersion = "1.7.4"
 
-    val http4sVersion    = "1.0.0-M32"
+    val http4sVersion    = "1.0.0-M42"
     val http4sJdkVersion = "1.0.0-M1"
     val avroVersion      = "1.11.3"
     val avro4sVersion    = "4.1.2"
