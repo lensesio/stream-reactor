70f6a2239ae7e332a1294714ecb9941728938478
Scala Steward <scala_steward@virtuslab.com>
2024-10-16 17:54:20 +0000

    Update mockito-junit-jupiter to 5.14.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -40,7 +40,7 @@ object Dependencies {
     val scalatestPlusScalaCheckVersion = "3.1.0.0-RC2"
     val scalaCheckVersion              = "1.18.1"
 
-    val mockitoJunitJupiterVersion = "5.14.1"
+    val mockitoJunitJupiterVersion = "5.14.2"
     val junitJupiterVersion        = "5.11.2"
     val assertjCoreVersion         = "3.26.3"
 
