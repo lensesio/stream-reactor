ee2644544e4cd85706aa7cd7d869f02d3463dc6c
Scala Steward <scala_steward@virtuslab.com>
2024-12-03 20:01:31 +0000

    Update s3, sts to 2.29.25

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -79,7 +79,7 @@ object Dependencies {
     val jerseyCommonVersion = "3.1.9"
 
     val calciteVersion = "1.34.0"
-    val awsSdkVersion  = "2.28.29"
+    val awsSdkVersion  = "2.29.25"
 
     val azureDataLakeVersion              = "12.21.0"
     val azureIdentityVersion              = "1.14.0"
