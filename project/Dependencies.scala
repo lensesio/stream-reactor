5bc8ccd7541f5986ba21ecb0fd802f6e13e32997
Scala Steward <scala_steward@virtuslab.com>
2024-12-21 17:34:44 +0000

    Update msal4j to 1.18.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -84,7 +84,7 @@ object Dependencies {
     val azureDataLakeVersion              = "12.22.0"
     val azureIdentityVersion              = "1.14.2"
     val azureCoreVersion                  = "1.54.1"
-    val msal4jVersion                     = "1.17.3"
+    val msal4jVersion                     = "1.18.0"
     val msal4jPersistenceExtensionVersion = "1.3.0"
     val gcpCloudVersion                   = "2.48.0"
     val gcpCloudStorageVersion            = "2.44.0"
