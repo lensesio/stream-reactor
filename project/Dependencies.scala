edb47fe7b08c3e410e019666ea99db73415795a5
Scala Steward <scala_steward@virtuslab.com>
2025-04-24 17:23:42 +0000

    Update protobuf-java to 3.25.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -116,7 +116,7 @@ object Dependencies {
     val jmsApiVersion         = "3.1.0"
     val activeMqVersion       = "6.1.4"
     val protocVersion         = "3.11.4"
-    val googleProtobufVersion = "3.25.5"
+    val googleProtobufVersion = "3.25.7"
 
     val mqttVersion = "1.2.5"
 
