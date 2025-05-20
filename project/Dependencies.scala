ea129686bc07effab9bed394d90fbfa3020ff1f0
Scala Steward <scala_steward@virtuslab.com>
2025-05-20 19:24:51 +0000

    Update cats-effect-testing-scalatest to 1.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -58,7 +58,7 @@ object Dependencies {
     val avro4sVersion    = "4.1.2"
 
     val catsEffectVersion     = "3.6.1"
-    val `cats-effect-testing` = "1.5.0"
+    val `cats-effect-testing` = "1.6.0"
 
     val antlr4Version: String = "4.13.2"
 
