46733519e499847bafae7db25386301f3f52f7f8
Scala Steward <scala_steward@virtuslab.com>
2025-06-05 23:33:59 +0000

    Update netty-codec, netty-codec-http, ... to 4.2.2.Final

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -102,7 +102,7 @@ object Dependencies {
     val lz4Version = "1.8.0"
 
     val bouncyCastleVersion = "1.81"
-    val nettyVersion        = "4.2.1.Final"
+    val nettyVersion        = "4.2.2.Final"
 
     val cassandraDriverVersion = "3.11.5"
     val jsonPathVersion        = "2.9.0"
