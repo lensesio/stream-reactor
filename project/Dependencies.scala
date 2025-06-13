eb86abfd153c6bca514e888628bbdbe7ab7974f8
Scala Steward <scala_steward@virtuslab.com>
2025-06-13 17:55:19 +0000

    Update kafka-connect-avro-converter, ... to 7.9.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -48,7 +48,7 @@ object Dependencies {
     val cyclopsVersion = "10.4.1"
 
     val kafkaVersion:     String = "3.9.1"
-    val confluentVersion: String = "7.9.1"
+    val confluentVersion: String = "7.9.2"
 
     val enumeratumVersion = "1.7.5"
 
