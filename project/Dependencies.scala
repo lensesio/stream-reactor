f95cffadbae41edf130dc8eec8b16fae3a732aa0
Scala Steward <scala_steward@virtuslab.com>
2025-07-13 15:48:25 +0000

    Update log4j-api, log4j-to-slf4j to 2.25.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -434,8 +434,8 @@ trait Dependencies {
   import Versions._
 
   val loggingDeps: Seq[ModuleID] = Seq(
-    "org.apache.logging.log4j" % "log4j-api"      % "2.25.0",
-    "org.apache.logging.log4j" % "log4j-to-slf4j" % "2.25.0",
+    "org.apache.logging.log4j" % "log4j-api"      % "2.25.1",
+    "org.apache.logging.log4j" % "log4j-to-slf4j" % "2.25.1",
     log4jToSlf4j,
     jclToSlf4j,
     logback,
