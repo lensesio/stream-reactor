54b3c530a5b2f0e088dc07f0bbe9f22f872ca85b
Scala Steward <scala_steward@virtuslab.com>
2025-07-23 16:28:47 +0000

    Update commons-codec to 1.19.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -122,7 +122,7 @@ object Dependencies {
     val mqttVersion = "1.2.5"
 
     val commonsNetVersion      = "3.11.1"
-    val commonsCodecVersion    = "1.18.0"
+    val commonsCodecVersion    = "1.19.0"
     val commonsCompressVersion = "1.27.1"
     val commonsConfigVersion   = "2.12.0"
     val commonsIOVersion       = "2.19.0"
