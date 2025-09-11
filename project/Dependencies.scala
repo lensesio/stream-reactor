d902ab9fae729bdcf52917b94befc2319d7effdd
Scala Steward <scala_steward@virtuslab.com>
2025-09-11 18:45:32 +0000

    Update gson to 2.13.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -142,7 +142,7 @@ object Dependencies {
     val mongoDbVersion = "3.12.14"
 
     val jedisVersion = "5.1.5"
-    val gsonVersion  = "2.13.1"
+    val gsonVersion  = "2.13.2"
 
     val classGraphVersion    = "4.8.179"
     val nimbusJoseJwtVersion = "10.3"
