9ca7f0fdf7456c0ae2cb8bb28b4733be4bc3b7cd
Scala Steward <scala_steward@virtuslab.com>
2025-09-17 17:03:20 +0000

    Update scala-logging to 3.9.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -71,7 +71,7 @@ object Dependencies {
     val lombokVersion = "1.18.38"
 
     val logbackVersion      = "1.5.18"
-    val scalaLoggingVersion = "3.9.5"
+    val scalaLoggingVersion = "3.9.6"
 
     val dnsJavaVersion  = "3.6.3"
     val wiremockVersion = "3.13.1"
