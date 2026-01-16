eb6f0e3bcaeb2fc2c8f68e8fab6948ff0d16df31
mkeskells <mike.skells@talk21.com>
2026-01-16 23:35:34 +0000

    use one operation only if exactlyOnce semantics (indexManager.indexingEnabled) are not enforced

--- kafka-connect-cloud-common/src/main/scala/io/lenses/streamreactor/connect/cloud/common/sink/writer/Writer.scala
+++ kafka-connect-cloud-common/src/main/scala/io/lenses/streamreactor/connect/cloud/common/sink/writer/Writer.scala
@@ -138,9 +138,21 @@ class Writer[SM <: FileMetadata](
         for {
           key         <- objectKeyBuilder.build(uncommittedOffset, earliestRecordTimestamp, latestRecordTimestamp)
           path        <- key.path.toRight(NonFatalCloudSinkError("No path exists within cloud location"))
-          pendingOperations = NonEmptyList.of[FileOperation](
-            UploadOperation(key.bucket, file, path),
-          )
+          pendingOperations = if (indexManager.indexingEnabled) {
+            val tempFileUuid = UUID.randomUUID().toString
+            val tempFileName = path.prependedAll(
+              s".temp-upload/${topicPartition.topic}/${topicPartition.partition}/$tempFileUuid",
+            )
+
+            NonEmptyList.of[FileOperation](
+              UploadOperation(key.bucket, file, tempFileName),
+              CopyOperation(key.bucket, tempFileName, path, "placeholder"),
+              DeleteOperation(key.bucket, tempFileName, "placeholder"),
+            )
+          } else
+            NonEmptyList.of[FileOperation](
+              UploadOperation(key.bucket, file, path)
+            )
 
           _ <- pendingOperationsProcessors.processPendingOperations(
             topicPartition,
